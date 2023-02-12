2d25c81cc40e58022f102620e58646061b5065ed
M W <mwilliams5772@hotmail.com>
2023-02-12 16:28:54 -0500

    Add function to add vertices to graph. Add field 'label' to vertices.

--- graphmst/src/graphs.rs
+++ graphmst/src/graphs.rs
@@ -8,17 +8,38 @@ pub struct Graph<V, E> {
 
 impl<V, E> Graph<V, E> {
     pub fn new() -> Graph<V, E> {
+        //Create an empty graph.
         let v: Vec<Vertex<V>> = Vec::new();
         let e: Vec<Edge<E>> = Vec::new();
         Graph::<V, E> {
             vertices: v,
-            edges: e,
+            edges: e
         }
     }
+    
+    pub fn add_vertex(&mut self, label: String, value: V) {
+        //Add vertex to graph.
+        if self.vertices.iter().any(|g| g.label.eq(&label)){
+            //TODO: Create more sophosticated handling.
+            println!("Vertex '{}' already in graph", label);
+        } else {
+            self.vertices.push(
+                Vertex {
+                    label: label,
+                    value: value
+                }
+            );            
+        }
+    }
+    
+    //pub fn add_edge(&mut self, e: (<label, label>)) { }
+    
+    //TODO: Add function to print graph.
 }
 
 struct Vertex<T> {
-    value: T,
+    label: String,
+    value: T
 }
 
 struct Edge<T> {
