e31d34176ea00903c65a9ea6345a3b34146529de
M W <mwilliams5772@hotmail.com>
2023-02-13 00:30:37 -0500

    Add function to add edges to graph.

--- graphmst/src/graphs.rs
+++ graphmst/src/graphs.rs
@@ -1,9 +1,12 @@
 //Contains definition of graph structures.
 
+
+type VLT = String; //vertex_label_type
+
 //Basic undirected graph.
 pub struct Graph<V, E> {
     vertices: Vec<Vertex<V>>,
-    edges: Vec<Edge<E>>,
+    edges: Vec<Edge<E>>
 }
 
 impl<V, E> Graph<V, E> {
@@ -17,7 +20,7 @@ impl<V, E> Graph<V, E> {
         }
     }
     
-    pub fn add_vertex(&mut self, label: String, value: V) {
+    pub fn add_vertex(&mut self, label: VLT, value: V) {
         //Add vertex to graph.
         if self.vertices.iter().any(|g| g.label.eq(&label)){
             //TODO: Create more sophosticated handling.
@@ -32,16 +35,60 @@ impl<V, E> Graph<V, E> {
         }
     }
     
-    //pub fn add_edge(&mut self, e: (<label, label>)) { }
+    pub fn add_edge(&mut self, e: (VLT, VLT), weight: E) {
+        if self.contains_edge(&e) {
+            println!("Edge '{}'-'{}' already in graph", e.0, e.1);
+        } else if
+            self.contains_vertex(&e.0)
+            && self.contains_vertex(&e.1) {
+                self.edges.push(
+                    Edge {
+                        endpoints: e,
+                        weight: weight
+                    }
+                )
+        }
+        /*
+        if self.contains_vertex(&e.0) && self.contains_vertex(&e.1) {
+            println!("{}, {}", e.0, e.1);
+
+        } else {
+            println!("Edge '{}'-'{}' already in graph", e.0, e.1);
+        }
+        */
+    }
+    
+    fn contains_vertex(&self, label: &VLT) -> bool {
+        //Check if graph contain vertex with label.
+        self.vertices.iter().any(|vert| vert.label.eq(label))
+    }
+    
+    fn contains_edge(&self, e: &(VLT, VLT)) -> bool {
+        //Check if graph contain an edge.
+        self.edges.iter().any(|edg| (edg.endpoints.0).eq(&e.0)
+        && (edg.endpoints.1).eq(&e.1))
+    }
     
     //TODO: Add function to print graph.
 }
 
 struct Vertex<T> {
-    label: String,
+    label: VLT,
     value: T
 }
 
+/*
+impl PartialEq for Vertex {
+    fn eq(&self, other: &Self) -> bool {
+        self.isbn == other.isbn
+    }
+}
+*/
+
 struct Edge<T> {
-    weight: T,
+    //endpoints: &'a (Vertex<V>, Vertex<V>),
+    //TODO: Change endpoints from vertex labels to vertex references.
+        // This requires adding lifetimes.
+    endpoints: (VLT, VLT),
+    weight: T
 }
