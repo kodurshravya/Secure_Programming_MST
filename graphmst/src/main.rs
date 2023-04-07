6c617de0c6f552d8e66f172583f1b0d96075e80f
shravyakodur <sk64669@uga.edu>
2023-04-06 22:27:15 -0400

    Revert "Implemented prims algorithm, Eq, Ord, PartialOrd"
    This reverts commit b8ef4f62fad4aa30eed0154019fd85f9992f1969.

--- graphmst/src/main.rs
+++ graphmst/src/main.rs
@@ -3,11 +3,7 @@ mod graphs;
 mod util;
 
 fn main() {
-    //let mut g = graphs::Graph::new(false);
     let mut g = graphs::Graph::new(false);
-    
-    
-
 
     g.add_vertex(String::from("A"), 0);
     g.add_vertex(String::from("B"), 1);
@@ -100,8 +96,7 @@ fn main() {
 
     // algos::Dijkstra(g, String::from("A"));
     println!("\n\n----KRUSKALS START -----\n\n");
-    //let mst_kruskals = algos::Kruskals(g);
-    let mst_kruskals = algos::Kruskals(&mut g);
+    let mst_kruskals = algos::Kruskals(g);
     match mst_kruskals {
         // Ok(g) => println!("MST generated successfully!"),
         Ok(g) => g.print(),
@@ -111,13 +106,4 @@ fn main() {
 
     // Kruskals(g, 5);
     //algos::BellmanFord(g, String::from("A"));
-    println!("\n\n----PRIMS START -----\n\n");
-    let mst_prims = algos::Prims(g);
-    //let mst_prims = algos::Prims(&mut g);
-    match mst_prims {
-        Ok(g) => g.print(),
-        Err(e) => println!("{}", e),
-    }
-    println!("\n\n----PRIMS END -----\n\n");
-
 }
